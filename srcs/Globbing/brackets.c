b514922f919010ede71fbd41a0b29faefaa16298
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-16 08:21:53 +0200

    auto commit

---
+++ srcs/Globbing/brackets.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brackets.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/22 12:03:22 by mafernan          #+#    #+#             */
+/*   Updated: 2018/07/16 08:03:51 by mafernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_shell.h"
+
+static int		braclose(const char *str, char c, int i, int b)
+{
+	while (b && *(++str) && (i++))
+	{
+		if (*str == c || *str == c + c % 2 + 1)
+		{
+			if (*str == c)
+				++b;
+			else
+				--b;
+		}
+	}
+	return (i);
+}
+
+int				brackets(const char *str, char c)
+{
+	if (*str == c)
+		return (1);
+	else if (!*str || *str == ')' || *str == '}' || *str == ']')
+		return (0);
+	else if (*str == '(' || *str == '{' || *str == '[')
+		return (brackets(str + 1, *str + *str % 2 + 1) *
+				brackets(str + braclose(str, *str, 1, 1), c));
+	else
+		return (brackets(str + 1, c));
+}
